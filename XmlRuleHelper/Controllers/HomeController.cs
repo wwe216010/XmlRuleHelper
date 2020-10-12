cf377fb03bd7178a62cced698310120bde94d111
Homer <Homer_WH_Chen@wistron.com>
2020-10-12 14:02:57 +0800

    20201012-commit for publish

--- XmlRuleHelper/Controllers/HomeController.cs
+++ XmlRuleHelper/Controllers/HomeController.cs
@@ -113,7 +113,7 @@ namespace XmlRuleHelper.Controllers
             IEnumerable<XElement> data = from c in purchaseOrder.Descendants() select c;
             var qq = data.OfType<XElement>().Attributes().Select(c => c.Name.LocalName).OrderBy(c => c.ToString());
             resJson += "<select class = \"form-control sel-attribute\" style=\"width:auto;\">";
-            resJson += "<option value=\"TagName\">元件名稱</option>";
+            resJson += "<option value=\"TagName\">節點名稱</option>";
             foreach (var itemAttr in qq.Distinct())
             {
                 resJson += string.Format("<option value=\"{0}\">{1}</option>", itemAttr, itemAttr);
